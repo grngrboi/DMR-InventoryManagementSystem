645680c432a4fb3a37952f43835aadee44015641
lemongoreng <eamonnnathanael@gmail.com>
2025-12-10 18:51:36 +0800

    Edited widget_test conts just for piece of mind hmm

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:daikin/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DMRInventoryApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
